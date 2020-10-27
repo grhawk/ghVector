1d670f0ecc6464b9887dd9950d5e7696178937d9
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2020-10-27 19:25:10 +0100

    [DEV] Adding some example of std::vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 
 #include <iostream>
-#include "example.h"
+#include <vector>
 
 int main () 
 {
-    std::cout << sayHello() << std::endl;
+    std::vector<int> v = std::vector<int>();
+    v.push_back(5);
+    v.push_back(6);
+    for(auto i=v.begin(); i != v.end(); i++ )
+        std::cout << "VALUE OF i: " << *i << std::endl;
     return 0;
 }
