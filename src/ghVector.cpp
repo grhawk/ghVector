940cb28a5510ab4d562fb441545fc1f6e982971f
Riccardo Petraglia <riccardo.petraglia@swisscom.com>
2020-10-30 07:49:21 +0100

    [DEV] Use a single file for the entire class (simplify the use of template).

--- src/ghVector.cpp
+++ src/ghVector.cpp
@@ -1,79 +1,91 @@
-#include "ghVector.h"
+#ifndef GH_VECTOR_ONCE_H
+#define GH_VECTOR_ONCE_H
+
 #include <string>
 
-template<typename T>
-ghds::uint ghds::Vector<T>::size() const noexcept {
-    return m_size;
-}
+namespace ghds {
+    typedef unsigned int uint;
 
-template<typename T>
-void ghds::Vector<T>::push_back(T value) noexcept {
-    if (m_size < m_capacity) {
-        m_array[m_size] = value;
-        m_size++;
-    } else {
-        ghds::uint new_capacity = computeNewCapacity();
-        T* new_array = new T[new_capacity];
-        std::memcpy (new_array, m_array, m_size * sizeof(T));
-        for (ghds::uint i = 0; i < m_size; i++){
-            new_array[i] = m_array[i];
+    template<typename T>
+    class Vector {
+    private:
+        uint m_size = 0;
+        uint m_capacity = 2;
+        T* m_array;
+
+    public:
+
+        uint size() const noexcept {
+            return m_size;
         }
-        delete [] m_array;
-        m_capacity = new_capacity;
-        m_array = new_array;
-        m_array[m_size] = value;
-        m_size++;
-    }
+
+        void push_back(T value) noexcept {
+            if (m_size < m_capacity) {
+                m_array[m_size] = value;
+                m_size++;
+            } else {
+                ghds::uint new_capacity = computeNewCapacity();
+                T* new_array = new T[new_capacity];
+                std::memcpy(new_array, m_array, m_size * sizeof(T));
+                for (ghds::uint i = 0; i < m_size; i++) {
+                    new_array[i] = m_array[i];
+                }
+                delete[] m_array;
+                m_capacity = new_capacity;
+                m_array = new_array;
+                m_array[m_size] = value;
+                m_size++;
+            }
+        }
+
+        T operator[](ghds::uint i) const {
+            return m_array[i];
+        }
+
+        Vector()
+                : m_array{new T[m_capacity]} {
+        }
+
+        Vector(T value) {
+            m_array = new T[m_capacity];
+            push_back(value);
+        }
+
+        T& operator[](ghds::uint i) {
+            return m_array[i];
+        }
+
+        ~Vector() {
+            delete[] m_array;
+        }
+
+        uint capacity() const noexcept {
+            return m_capacity;
+        }
+
+        void insert(ghds::uint position, T value) {
+            if (position >= m_size)
+                throw std::exception();
+            ghds::uint new_capacity = m_capacity;
+            if (m_size == m_capacity)
+                new_capacity = computeNewCapacity();
+            T* new_array = new T[new_capacity];
+            std::memcpy(new_array, m_array, (position) * sizeof(T));
+            new_array[position] = value;
+            m_size++;
+            std::memcpy(&new_array[position + 1], &m_array[position], (m_size - position) * sizeof(T));
+
+            delete[] m_array;
+            m_array = new_array;
+            m_capacity = new_capacity;
+        }
+
+    private:
+        uint computeNewCapacity() const {
+            return this->m_capacity + (this->m_capacity / 2);;
+        }
+
+    };
 }
 
-template<typename T>
-ghds::uint ghds::Vector<T>::computeNewCapacity() const {
-    return this->m_capacity + (this->m_capacity / 2);;
-}
-
-template<typename T>
-T ghds::Vector<T>::operator[](ghds::uint i) const {
-    return m_array[i];
-}
-
-template<typename T>
-ghds::Vector<T>::Vector()
-        : m_array{new T[m_capacity]}
-{
-}
-
-template<typename T>
-T &ghds::Vector<T>::operator[](ghds::uint i) {
-    return m_array[i];
-}
-
-template<typename T>
-ghds::Vector<T>::~Vector() {
-    delete[] m_array;
-}
-
-template<typename T>
-ghds::uint ghds::Vector<T>::capacity() const noexcept {
-    return m_capacity;
-}
-
-template<typename T>
-void ghds::Vector<T>::insert(ghds::uint position, T value) {
-    if (position >= m_size)
-        throw std::exception();
-    ghds::uint new_capacity = m_capacity;
-    if(m_size == m_capacity)
-        new_capacity = computeNewCapacity();
-    T* new_array = new T[new_capacity];
-    std::memcpy(new_array, m_array, (position) * sizeof(T));
-    new_array[position] = value;
-    m_size++;
-    std::memcpy(&new_array[position+1], &m_array[position], (m_size - position) * sizeof(T));
-
-    delete [] m_array;
-    m_array = new_array;
-    m_capacity = new_capacity;
-}
-
-
-template class ghds::Vector<int>;
+#endif
